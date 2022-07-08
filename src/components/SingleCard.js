35745ec96643022e879b74a9824d93cc430e9dac
saihtutkhaungmyint <saihtutkhaungmyint@gmail.com>
2022-07-09 00:08:11 +0630

    React Memory Game

---
+++ src/components/SingleCard.js
@@ -0,0 +1,24 @@
+import './SingleCard.css';
+const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className='card'>
+      <div className={flipped ? 'flipped' : ''}>
+        <img src={card.src} className='front' alt='card-front' />
+        <img
+          className='back'
+          src='/img/cover.png'
+          alt='card-back'
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SingleCard;
